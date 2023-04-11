8e122cc4ac3d7325d6934a936cc9d71c090c322b
Bruno Pereira Tiburcio Rodrigues <bruno.rodrigues@potencial.corp>
2023-04-11 11:32:04 -0300

    commit v0.0.2

---
+++ android/app/src/main/kotlin/com/example/appwebview/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.appwebview
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late WebViewController _webViewController;
+  String _url =
+      'http://chamados.potencialflorestal.com.br/glpi/marketplace/formcreator/front/formdisplay.php?id=1';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Chamados Potencial Florestal'),
+          backgroundColor: Color.fromRGBO(0, 168, 89, 1),
+          actions: [
+            IconButton(
+                onPressed: () {
+                  _closeApp();
+                },
+                icon: Icon(Icons.close)),
+          ],
+        ),
+        // floatingActionButton: FloatingActionButton.extended(
+        //   backgroundColor: Color.fromRGBO(0, 168, 89, 1),
+        //   label: Text("Home"),
+        //   icon: Icon(Icons.home),
+        //   onPressed: () {
+        //     _webViewController.loadUrl(_url);
+        //   },
+        // ),
+        body: _body());
+  }
+
+  _body() {
+    return Container(
+      // padding: EdgeInsets.only(top: 32),
+      // margin: EdgeInsets.only(top: 32),
+      child: WebView(
+        initialUrl: _url,
+        javascriptMode: JavascriptMode.unrestricted,
+        onWebViewCreated: (WebViewController webViewController) {
+          _webViewController = webViewController;
+        },
+      ),
+    );
+  }
+
+  void _closeApp() {
+    SystemNavigator.pop();
+  }
+}
